c6878f46c948b6129d27677457231c3dce0510e4
JushuaC <1047280552@qq.com>
2024-05-27 23:00:38 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -212,23 +212,19 @@ def batchpowernorm4(scope, signal, momentum=0.999, mode='train'):
 def batchpowernorm(scope, signal, momentum=0.999, mode='train'):
     gamma = scope.param('gamma', lambda rng, shape: jnp.ones(shape), signal.val.shape[-1])
     beta = scope.param('beta', lambda rng, shape: jnp.zeros(shape), signal.val.shape[-1])
+    tau = scope.param('tau', lambda rng, shape: jnp.zeros(shape), signal.val.shape[-1])
     
-    # running_mean = scope.variable('norm', 'running_mean', lambda *_: 0. + jnp.ones(signal.val.shape[-1]), ())
-    running_mean = scope.param('norm', lambda rng, shape: jnp.ones(shape), signal.val.shape[-1])
-    # running_var = scope.variable('norm', 'running_var', lambda *_: 0. + jnp. s(signal.val.shape[-1]), ())
+    running_mean = scope.variable('norm', 'running_mean', lambda *_: 0. + jnp.ones(signal.val.shape[-1]), ())
     if mode == 'train':
-        mean = jnp.mean(jnp.abs(signal.val)**2, axis=-1, keepdims=True)
-        var = jnp.var(jnp.abs(signal.val)**2, axis=0) 
-        running_mean = momentum * running_mean + (1 - momentum) * mean
-        # running_var.value = momentum * running_var.value + (1 - momentum) * var
-        
+        mean = jnp.mean(jnp.abs(signal.val)**2, axis=0, keepdims=True)
+        running_mean.value = momentum * running_mean.value + (1 - momentum) * mean
     else:
         mean = running_mean
-        # var = running_var.value
-
+      
+    s = signal / jnp.sqrt(mean)
     # scaled_signal = normalized_signal * gamma + beta
 
-    return (signal / jnp.sqrt(mean))
+    return s
 
 def conv1d(
     scope: Scope,
