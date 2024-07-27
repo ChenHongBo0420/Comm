301b651df530986101f39e5a49bd4b0702107f4d
JushuaC <1047280552@qq.com>
2024-07-27 16:09:20 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -340,7 +340,7 @@ def fdbp(
     dconv = vmap(wpartial(conv1d, taps=dtaps, kernel_init=d_init))
     for i in range(steps):
         x, td = scope.child(dconv, name='DConv_%d' % i)(Signal(x, t))
-        x = jax.nn.relu(x)
+        x = jnp.tanh(x)
         c, t = scope.child(mimoconv1d, name='NConv_%d' % i)(Signal(jnp.abs(x)**2, td),
                                                             taps=ntaps,
                                                             kernel_init=n_init)
