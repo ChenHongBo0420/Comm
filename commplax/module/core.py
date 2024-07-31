212340df952a594457055d5da60a68daf1ae5a3a
JushuaC <1047280552@qq.com>
2024-07-31 14:24:27 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -388,14 +388,15 @@ def fdbp(
     key = random.PRNGKey(0)
     dconv = vmap(wpartial(conv1d, taps=dtaps, kernel_init=d_init))
     for i in range(steps):
-        x = energy_attention(x)
+        # x = energy_attention(x)
         x, td = scope.child(dconv, name='DConv_%d' % i)(Signal(x, t))
+        x1 = x
         c, t = scope.child(mimoconv1d, name='NConv_%d' % i)(Signal(jnp.abs(x)**2, td),
                                                             taps=ntaps,
                                                             kernel_init=n_init)
 
         x = jnp.exp(1j * c) * x[t.start - td.start: t.stop - td.stop + x.shape[0]]
-        
+        x = x + x1
         # Apply channel shuffle with GRU
         batch_size, channels = x.shape
         # x = energy_attention(x)
