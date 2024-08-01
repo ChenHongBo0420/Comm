c9cbe3bff5ec7100a4ea315decaa664f08fe9386
JushuaC <1047280552@qq.com>
2024-08-02 04:23:49 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -400,8 +400,9 @@ def fdbp(
     x, t = signal
     key = random.PRNGKey(0)
     dconv = vmap(wpartial(conv1d, taps=dtaps, kernel_init=d_init))
+    x = complex_channel_attention(x)
     for i in range(steps):
-        x = complex_channel_attention(x)
+        # x = complex_channel_attention(x)
         x, td = scope.child(dconv, name='DConv_%d' % i)(Signal(x, t))
         c, t = scope.child(mimoconv1d, name='NConv_%d' % i)(Signal(jnp.abs(x)**2, td),
                                                             taps=ntaps,
