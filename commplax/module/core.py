2adfd7be618d9cf9401e401a603974055565f576
JushuaC <1047280552@qq.com>
2024-08-03 04:20:00 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -393,7 +393,7 @@ def fdbp(
         c, t = scope.child(mimoconv1d, name='NConv_%d' % i)(Signal(jnp.abs(x)**2, td),
                                                             taps=ntaps,
                                                             kernel_init=n_init)
-        x = residual_block(x, complex_channel_attention, alpha=0.5)
+        x = complex_channel_attention(x)
         x = jnp.exp(1j * c) * x[t.start - td.start: t.stop - td.stop + x.shape[0]]
         
         
