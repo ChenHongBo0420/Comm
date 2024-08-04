0f0e962c3e5cb6c59bfa9d19bd6739b3f93e3a44
JushuaC <1047280552@qq.com>
2024-08-05 05:00:26 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -432,7 +432,7 @@ def fdbp(
         c, t = scope.child(mimoconv1d, name=f'NConv_{i}')(Signal(jnp.abs(x)**2, td), taps=ntaps, kernel_init=n_init)
 
         h = jnp.zeros_like(c)
-        h = gate(c, h)
+        c = gate(c, h)
         
         x = jnp.exp(1j * c) * x[t.start - td.start: t.stop - td.stop + x.shape[0]]
         
