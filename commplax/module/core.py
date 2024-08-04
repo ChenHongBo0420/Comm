9b9897592af792bc48cb284d3d32ad281da2f0a0
JushuaC <1047280552@qq.com>
2024-08-04 18:14:28 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -413,7 +413,7 @@ def fdbp(
         x, td = scope.child(dconv, name=f'DConv_{i}')(Signal(x, t))
         c, t = scope.child(mimoconv1d, name=f'NConv_{i}')(Signal(jnp.abs(x)**2, td), taps=ntaps, kernel_init=n_init)
         # x = se_attention(x)
-        c = csqueeze_excite_attention(c)
+        c = squeeze_excite_attention(c)
         x = jnp.exp(1j * c) * x[t.start - td.start: t.stop - td.stop + x.shape[0]]
         
     return Signal(x, t)
