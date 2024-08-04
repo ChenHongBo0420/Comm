5df901c60d6990e23d46f0150541aab80cfb7e6e
JushuaC <1047280552@qq.com>
2024-08-04 21:04:01 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -412,10 +412,11 @@ def fdbp(
         
         x, td = scope.child(dconv, name=f'DConv_{i}')(Signal(x, t))
         c, t = scope.child(mimoconv1d, name=f'NConv_{i}')(Signal(jnp.abs(x)**2, td), taps=ntaps, kernel_init=n_init)
-        t_encoded = jnp.linspace(t.start, t.stop, c.shape[0])[:, None]
-        c_with_t = jnp.concatenate([x, t_encoded], axis=1)
-        x = encoder(c_with_t)
-        x = x[:, :-1]
+      
+        t_encoded = jnp.linspace(td.start, td.stop, x.shape[0])[:, None]
+        c = jnp.concatenate([c, t_encoded], axis=1)
+        c, hidden_state = encoder(c, hidden_state=t_encoded)
+      
         # c = complex_channel_attention(c)
       
         x = jnp.exp(1j * c) * x[t.start - td.start: t.stop - td.stop + x.shape[0]]
