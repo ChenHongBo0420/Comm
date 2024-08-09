362cd51aaefa1f5800523dae8e07b6eb84b07e4a
JushuaC <1047280552@qq.com>
2024-08-10 04:49:51 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -511,27 +511,28 @@ def fdbp(
     
     for i in range(steps):
         # 独立处理 Signal(x, t)
-        x, td = scope.child(dconv, name='DConv_%d_x' % i)(Signal(x, t))
-        c_x, t_x = scope.child(mimoconv1d, name='NConv_%d_x' % i)(Signal(jnp.abs(x)**2, td),
-                                                                  taps=ntaps,
-                                                                  kernel_init=n_init)
-        x = jnp.exp(1j * c_x) * x[t_x.start - td.start: t_x.stop - td.stop + x.shape[0]]
+        # x, td = scope.child(dconv, name='DConv_%d_x' % i)(Signal(x, t))
+        # c_x, t_x = scope.child(mimoconv1d, name='NConv_%d_x' % i)(Signal(jnp.abs(x)**2, td),
+        #                                                           taps=ntaps,
+        #                                                           kernel_init=n_init)
+        # x = jnp.exp(1j * c_x) * x[t_x.start - td.start: t_x.stop - td.stop + x.shape[0]]
         
         # 独立处理 Signal(hidden_state1, t)
-        hs1, _ = scope.child(dconv, name='DConv_%d_hs1' % i)(Signal(hidden_state1, t))
-        c_hs1, _ = scope.child(mimoconv1d, name='NConv_%d_hs1' % i)(Signal(jnp.abs(hs1)**2, td),
+        hs1, td1 = scope.child(dconv, name='DConv_%d_hs1' % i)(Signal(hidden_state1, t))
+        c_hs1, t_hs1 = scope.child(mimoconv1d, name='NConv_%d_hs1' % i)(Signal(jnp.abs(hs1)**2, td1),
                                                                         taps=ntaps,
                                                                         kernel_init=n_init)
-        hidden_state1 = jnp.exp(1j * c_hs1) * hs1[t_x.start - td.start: t_x.stop - td.stop + hs1.shape[0]]
+        hidden_state1 = jnp.exp(1j * c_hs1) * hs1[t_hs1.start - td1.start: t_hs1.stop - td1.stop + hs1.shape[0]]
         
         # 独立处理 Signal(hidden_state2, t)
-        hs2, _ = scope.child(dconv, name='DConv_%d_hs2' % i)(Signal(hidden_state2, t))
-        c_hs2, _ = scope.child(mimoconv1d, name='NConv_%d_hs2' % i)(Signal(jnp.abs(hs2)**2, td),
-                                                                        taps=ntaps,
-                                                                        kernel_init=n_init)
-        hidden_state2 = jnp.exp(1j * c_hs2) * hs2[t_x.start - td.start: t_x.stop - td.stop + hs2.shape[0]]
-     
-    final_output = x + hidden_state1 + hidden_state2
+        # hs2, td2 = scope.child(dconv, name='DConv_%d_hs2' % i)(Signal(hidden_state2, t))
+        # c_hs2, t_hs2 = scope.child(mimoconv1d, name='NConv_%d_hs2' % i)(Signal(jnp.abs(hs2)**2, td2),
+        #                                                                 taps=ntaps,
+        #                                                                 kernel_init=n_init)
+        # hidden_state2 = jnp.exp(1j * c_hs2) * hs2[t_hs2.start - td2.start: t_hs2.stop - td2.stop + hs2.shape[0]]
+
+    # 最后进行融合
+    final_output = hidden_state1
     
     return Signal(final_output, t)
       
