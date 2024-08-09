60af118792826ef45fe343fe181dee86a3fc356d
JushuaC <1047280552@qq.com>
2024-08-10 04:40:33 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -519,14 +519,14 @@ def fdbp(
         
         # 独立处理 Signal(hidden_state1, t)
         hs1, _ = scope.child(dconv, name='DConv_%d_hs1' % i)(Signal(hidden_state1, t))
-        c_hs1, _ = scope.child(mimoconv1d, name='NConv_%d_hs1' % i)(Signal(jnp.abs(hs1)**2, td1),
+        c_hs1, _ = scope.child(mimoconv1d, name='NConv_%d_hs1' % i)(Signal(jnp.abs(hs1)**2, td),
                                                                         taps=ntaps,
                                                                         kernel_init=n_init)
         hidden_state1 = jnp.exp(1j * c_hs1) * hs1[t_x.start - td.start: t_x.stop - td.stop + hs1.shape[0]]
         
         # 独立处理 Signal(hidden_state2, t)
         hs2, _ = scope.child(dconv, name='DConv_%d_hs2' % i)(Signal(hidden_state2, t))
-        c_hs2, _ = scope.child(mimoconv1d, name='NConv_%d_hs2' % i)(Signal(jnp.abs(hs2)**2, td2),
+        c_hs2, _ = scope.child(mimoconv1d, name='NConv_%d_hs2' % i)(Signal(jnp.abs(hs2)**2, td),
                                                                         taps=ntaps,
                                                                         kernel_init=n_init)
         hidden_state2 = jnp.exp(1j * c_hs2) * hs2[t_x.start - td.start: t_x.stop - td.stop + hs2.shape[0]]
