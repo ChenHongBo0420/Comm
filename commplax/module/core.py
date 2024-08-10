f0da3f2180c41c1fc2393124c623aeca3342aab9
JushuaC <1047280552@qq.com>
2024-08-10 18:30:50 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -495,7 +495,7 @@ class TwoLayerRNN:
         # 观测方程
         output = jnp.dot(hidden_state2, self.C)
         
-        return output, hidden_state1, hidden_state2
+        return output
       
 class LinearLayer:
     def __init__(self, input_dim, output_dim):
