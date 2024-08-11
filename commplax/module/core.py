7c5836966dfc5278d8ec260e341bf5127cfa5477
JushuaC <1047280552@qq.com>
2024-08-11 17:13:27 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -464,40 +464,6 @@ def generate_hippo_matrix(size):
     A = -2.0 * jnp.tril(jnp.ones((n, n)), -1) + jnp.diag(P)
     return A
 
-# class TwoLayerRNN:
-#     def __init__(self, input_dim, hidden_size1, hidden_size2, output_dim):
-#         self.hidden_size1 = hidden_size1
-#         self.hidden_size2 = hidden_size2
-
-#         # 使用 HIPPO 矩阵初始化状态转移矩阵 A
-#         self.A1 = generate_hippo_matrix(hidden_size1)
-#         self.A2 = generate_hippo_matrix(hidden_size2)
-        
-#         # 输入矩阵 B
-#         self.B1 = orthogonal()(random.PRNGKey(1), (input_dim, hidden_size1))
-#         self.B2 = orthogonal()(random.PRNGKey(2), (hidden_size1, hidden_size2))
-
-#         # 观测矩阵 C
-#         self.C = orthogonal()(random.PRNGKey(3), (hidden_size2, output_dim))
-    
-#     def __call__(self, x, hidden_state1=None, hidden_state2=None):
-#         if hidden_state1 is None:
-#             hidden_state1 = jnp.zeros((x.shape[0], self.hidden_size1))
-#         if hidden_state2 is None:
-#             hidden_state2 = jnp.zeros((x.shape[0], self.hidden_size2))
-        
-#         # 使用 HIPPO 矩阵进行状态更新并应用注意力机制
-#         hidden_state1 = jnp.dot(hidden_state1, self.A1) + jnp.dot(x, self.B1)
-#         hidden_state1 = complex_channel_attention(hidden_state1)  # 应用注意力机制
-
-#         hidden_state2 = jnp.dot(hidden_state2, self.A2) + jnp.dot(hidden_state1, self.B2)
-#         hidden_state2 = squeeze_excite_attention(hidden_state2)  # 应用注意力机制
-        
-#         # 观测方程
-#         output = jnp.dot(hidden_state2, self.C)
-        
-#         return output
-
 class TwoLayerRNN:
     def __init__(self, input_dim, hidden_size1, hidden_size2, output_dim):
         self.hidden_size1 = hidden_size1
@@ -519,22 +485,19 @@ class TwoLayerRNN:
             hidden_state1 = jnp.zeros((x.shape[0], self.hidden_size1))
         if hidden_state2 is None:
             hidden_state2 = jnp.zeros((x.shape[0], self.hidden_size2))
-
-        # 对 A1 矩阵应用注意力机制，然后更新 hidden_state1
-        A1_attention = squeeze_excite_attention(self.A1)
-        hidden_state1 = jnp.dot(hidden_state1, A1_attention) + jnp.dot(x, self.B1)
+        
+        # 使用 HIPPO 矩阵进行状态更新并应用注意力机制
+        hidden_state1 = jnp.dot(hidden_state1, self.A1) + jnp.dot(x, self.B1)
         hidden_state1 = complex_channel_attention(hidden_state1)  # 应用注意力机制
 
-        # 对 A2 矩阵应用注意力机制，然后更新 hidden_state2
-        A2_attention = squeeze_excite_attention(self.A2)
-        hidden_state2 = jnp.dot(hidden_state2, A2_attention) + jnp.dot(hidden_state1, self.B2)
+        hidden_state2 = jnp.dot(hidden_state2, self.A2) + jnp.dot(hidden_state1, self.B2)
         hidden_state2 = squeeze_excite_attention(hidden_state2)  # 应用注意力机制
         
         # 观测方程
         output = jnp.dot(hidden_state2, self.C)
-        output = complex_channel_attention(output)  # 对最终输出也应用注意力机制
         
         return output
+
       
 class LinearLayer:
     def __init__(self, input_dim, output_dim):
