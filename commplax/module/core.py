0e1b09dbf921aada35c15bcb8334ffa014af684a
JushuaC <1047280552@qq.com>
2024-08-12 10:36:16 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -520,14 +520,13 @@ def fdbp(
     input_dim = x.shape[1]
     hidden_size = 2  
     output_dim = x.shape[1]
-    rnn_layer = TwoLayerRNN(input_dim, hidden_size, hidden_size, output_dim)
-    x = rnn_layer(x)
+    # rnn_layer = TwoLayerRNN(input_dim, hidden_size, hidden_size, output_dim)
+    # x = rnn_layer(x)
     for i in range(steps):
         x, td = scope.child(dconv, name='DConv_%d' % i)(Signal(x, t))
         c, t = scope.child(mimoconv1d, name='NConv_%d' % i)(Signal(jnp.abs(x)**2, td),
                                                             taps=ntaps,
                                                             kernel_init=n_init)
-        # x = complex_channel_attention(x)
         x = jnp.exp(1j * c) * x[t.start - td.start: t.stop - td.stop + x.shape[0]]
     return Signal(x, t)
       
