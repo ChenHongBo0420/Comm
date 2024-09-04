d079f592f4d8df75e643c768913fa65a3ca12a92
JushuaC <1047280552@qq.com>
2024-09-05 01:40:03 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -512,12 +512,19 @@ class LinearLayer:
 #     x2_updated = x2 + weight * x1  # 加权求和
 #     return x1_updated, x2_updated
 
-def weighted_interaction(x1, x2):
-    x1_normalized = (x1 - jnp.mean(x1)) / (jnp.std(x1) + 1e-6)
-    x2_normalized = (x2 - jnp.mean(x2)) / (jnp.std(x2) + 1e-6)
-    weight = jnp.mean(x1_normalized * x2_normalized)
-    x1_updated = x1 + weight * x2
-    x2_updated = x2 + weight * x1
+# def weighted_interaction(x1, x2):
+#     x1_normalized = (x1 - jnp.mean(x1)) / (jnp.std(x1) + 1e-6)
+#     x2_normalized = (x2 - jnp.mean(x2)) / (jnp.std(x2) + 1e-6)
+#     weight = jnp.mean(x1_normalized * x2_normalized)
+#     x1_updated = x1 + weight * x2
+#     x2_updated = x2 + weight * x1
+#     return x1_updated, x2_updated
+
+def weighted_interaction(x1, x2, decay_factor=0.9):
+    weight = jnp.mean(x1 * x2)
+    decayed_weight = weight * decay_factor  # 递减加权
+    x1_updated = x1 + decayed_weight * x2
+    x2_updated = x2 + decayed_weight * x1
     return x1_updated, x2_updated
 
 
