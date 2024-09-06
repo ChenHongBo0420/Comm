14befb1c46d0d8c70d350e2cbc7014451c499208
JushuaC <1047280552@qq.com>
2024-09-07 04:16:43 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -565,7 +565,8 @@ def fdbp(
     x, t = signal
     dconv = vmap(wpartial(conv1d, taps=dtaps, kernel_init=d_init))
     input_dim = x.shape[1]
-    hidden_size = 2  
+    # hidden_size = 2 
+    hidden_size = [2, 2]
     output_dim = x.shape[1]
     x1 = x[:, 0]
     x2 = x[:, 1]
