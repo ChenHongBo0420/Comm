2ca9b74eabb76268d436529273f5331f23a4d640
JushuaC <1047280552@qq.com>
2024-09-29 11:48:02 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -540,7 +540,7 @@ def fdbp(
     x_updated = jnp.stack([x1_updated, x2_updated], axis=1)
     rnn_layer = TwoLayerRNN(input_dim, hidden_size, hidden_size, output_dim)
     rnn_layer1 = TwoLayerRNN(input_dim, hidden_size, hidden_size, output_dim)
-    # x = rnn_layer(x_updated)
+    x = rnn_layer(x_updated)
     for i in range(steps):
         x, td = scope.child(dconv, name='DConv_%d' % i)(Signal(x, t))
         x = rnn_layer1(x)
