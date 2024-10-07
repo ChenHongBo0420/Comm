aa54224cb0945442cbf4ec6755c79f7c7bc6d11f
JushuaC <1047280552@qq.com>
2024-10-07 22:51:11 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -660,15 +660,22 @@ def fdbp1(
     n_init=gauss):
     x, t = signal
     dconv = vmap(wpartial(conv1d, taps=dtaps, kernel_init=d_init))
-    input_dim = 2
+    input_dim = x.shape[1]
     hidden_size = 2 
-    output_dim = 2
+    output_dim = x.shape[1]
     x1 = x[:, 0]
     x2 = x[:, 1]
     x1_updated, x2_updated = weighted_interaction(x1, x2)
     x_updated = jnp.stack([x1_updated, x2_updated], axis=1)
     rnn_layer = TwoLayerRNN(input_dim, hidden_size, hidden_size, output_dim)
     x = rnn_layer(x_updated)
+    for i in range(steps):
+        x, td = scope.child(dconv, name='DConv_%d' % i)(Signal(x, t))
+        c, t = scope.child(mimoconv1d, name='NConv_%d' % i)(Signal(jnp.abs(x)**2, td),
+                                                            taps=ntaps,
+                                                            kernel_init=n_init)
+        x = jnp.exp(1j * c) * x[t.start - td.start: t.stop - td.stop + x.shape[0]]
+    
     return Signal(x, t)
       
 def identity(scope, inputs):
