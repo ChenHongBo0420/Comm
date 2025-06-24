52c3ec55ab892376ae89e09844f82d09dac8696f
JushuaC <1047280552@qq.com>
2025-06-24 19:32:29 +0900

    Update __init__.py

--- commplax/__init__.py
+++ commplax/__init__.py
@@ -1,3 +1,4 @@
 from .version import __version__
 from .coding.qc_ldpc_ste import qc_ldpc_encode, init_G_soft
 from .coding.qam16_mapper import bits_to_sym, sym_to_bits
+from .coding.FEC import tx_pipeline, bit_bce_loss, init_G_soft, NeuralBP, build_optimizers
