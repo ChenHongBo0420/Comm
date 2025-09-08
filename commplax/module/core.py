f77865b93d12d40a8136c88289341b25d531b7f4
JushuaC <1047280552@qq.com>
2025-09-09 03:05:48 +0900

    Update core.py

--- commplax/module/core.py
+++ commplax/module/core.py
@@ -194,22 +194,22 @@ def batchpowernorm(scope, signal, momentum=0.999, mode='train'):
     return signal / jnp.sqrt(mean)
 
 
-# def conv1d(
-#     scope: Scope,
-#     signal,
-#     taps=31,
-#     rtap=None,
-#     mode='valid',
-#     kernel_init=delta,
-#     conv_fn = xop.convolve):
+def conv1d(
+    scope: Scope,
+    signal,
+    taps=31,
+    rtap=None,
+    mode='valid',
+    kernel_init=delta,
+    conv_fn = xop.convolve):
 
-#     x, t = signal
-#     t = scope.variable('const', 't', conv1d_t, t, taps, rtap, 1, mode).value
-#     h = scope.param('kernel',
-#                      kernel_init,
-#                      (taps,), np.complex64)
-#     x = conv_fn(x, h, mode=mode)
-#     return Signal(x, t)
+    x, t = signal
+    t = scope.variable('const', 't', conv1d_t, t, taps, rtap, 1, mode).value
+    h = scope.param('kernel',
+                     kernel_init,
+                     (taps,), np.complex64)
+    x = conv_fn(x, h, mode=mode)
+    return Signal(x, t)
 # ===== 工况：在 aux_inputs 里统一注册/读取（有默认值） =====
 
 def _aux(scope: Scope, key: str, default):
